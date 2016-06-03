f1f6eb4720d2a47690aa22eaa0bc7e6105bb5dae
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-03 12:33:57 +1000

    Add boilerplate

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Gdk",
     dependencies: [
+        .Package(url: "https://github.com/rhx/CGdk.git", majorVersion: 1),
+        .Package(url: "https://github.com/rhx/SwiftGdkPixbuf.git", majorVersion: 2),
         .Package(url: "https://github.com/rhx/SwiftPangoCairo.git", majorVersion: 2)
     ]
-)
 )
