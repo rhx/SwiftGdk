28be2549f2a444d2d0b1d8c6b400a19a02846019
Rene Hexel <r.hexel@griffith.edu.au>
2016-07-31 14:38:48 +1000

    Compile with Swift 3.0 preview 3

--- config.sh
+++ config.sh
@@ -7,6 +7,6 @@ Mod=Gdk
 Module=${Mod}-3.0
 module=`echo "${Module}" | tr '[:upper:]' '[:lower:]'`
 mod=`echo "${Mod}" | tr '[:upper:]' '[:lower:]'`
-export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
+export PATH=`pwd`/.build/gir2swift/.build/debug:"${PATH}"
 LINKFLAGS=`pkg-config --libs $module pangocairo pangoft2 pango gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $module pangocairo pangoft2 pango gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
