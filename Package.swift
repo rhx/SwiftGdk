44809cbd01c2e05f6259ebfefdcc4b216e04325a
Rene Hexel <r.hexel@griffith.edu.au>
2017-09-28 07:48:23 +1000

    Compile with SwiftLanguageVersion 3 under Swift 4

--- Package.swift
+++ Package.swift
@@ -6,5 +6,6 @@ let package = Package(
         .Package(url: "https://github.com/rhx/CGdk.git", majorVersion: 1),
         .Package(url: "https://github.com/rhx/SwiftGdkPixbuf.git", majorVersion: 2),
         .Package(url: "https://github.com/rhx/SwiftPangoCairo.git", majorVersion: 2)
-    ]
+    ],
+    swiftLanguageVersions: [3]
 )
