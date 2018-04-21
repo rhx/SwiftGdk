ff336cc87611269f307338623f9b862e0eafb46c
Rene Hexel <r.hexel@griffith.edu.au>
2018-04-22 06:37:45 +1000

    Make this compile with Swift 4

--- Package.swift
+++ Package.swift
@@ -7,5 +7,5 @@ let package = Package(
         .Package(url: "https://github.com/rhx/SwiftGdkPixbuf.git", majorVersion: 2),
         .Package(url: "https://github.com/rhx/SwiftPangoCairo.git", majorVersion: 2)
     ],
-    swiftLanguageVersions: [3]
+    swiftLanguageVersions: [3, 4]
 )
