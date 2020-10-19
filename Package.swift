91c44f5568d38fa8f2ee9eeb840420aa0062df3a
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-10-20 01:46:34 +0200

    Remove the need for build bash

--- Package.swift
+++ Package.swift
@@ -6,11 +6,11 @@ let package = Package(
     name: "Gdk",
     products: [ .library(name: "Gdk", targets: ["Gdk"]) ],
     dependencies: [
-        .package(name: "GdkPixbuf", url: "https://github.com/rhx/SwiftGdkPixbuf.git", .branch("master")),
-        .package(name: "PangoCairo", url: "https://github.com/rhx/SwiftPangoCairo.git", .branch("master")),
+        .package(name: "GdkPixbuf", url: "https://github.com/mikolasstuchlik/SwiftGdkPixbuf.git", .branch("master")),
+        .package(name: "PangoCairo", url: "https://github.com/mikolasstuchlik/SwiftPangoCairo.git", .branch("master")),
     ],
     targets: [
-	.systemLibrary(name: "CGdk", pkgConfig: "gdk-3.0 pangocairo pangoft2 pango gio-unix-2.0 glib-2.",
+	.systemLibrary(name: "CGdk", pkgConfig: "gdk-3.0",
 	    providers: [
 		.brew(["gtk+3", "glib", "glib-networking", "gobject-introspection"]),
 		.apt(["libgtk-3-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
