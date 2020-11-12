5bfd77bb8fe13d3c11b7e912a48d4ede5168d048
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-11-12 17:58:23 +0100

    Add and remove files for building queue

--- distclean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Remove Packages directory and generated files
-#
-. ./config.sh
-./clean.sh
-exec rm -rf Package.pins Packages Package.resolved Sources/${Mod}/*-*.swift Sources/${Mod}/${Module}.swift Sources/${Mod}/Swift${Mod}.swift ${Mod}.xcodeproj
